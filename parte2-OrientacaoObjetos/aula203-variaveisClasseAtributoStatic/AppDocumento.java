67ccb4361660c144f05da58f817ffdb3161ad556
Igor Dias <igordias.md@gmail.com>
2026-08-27 04:36:38 -0300

    AppDocumento Aula 203

--- parte2-OrientacaoObjetos/aula203-variaveisClasseAtributoStatic/AppDocumento.java
+++ parte2-OrientacaoObjetos/aula203-variaveisClasseAtributoStatic/AppDocumento.java
@@ -1,5 +1,5 @@
-public class AppDocumento{
-    public static void main(String args[]){
+public class AppDocumento {
+    public static void main(String[] args) {
         Documento d1, d2, d3;
 
         d1 = new Documento(1, "Procuracao");
@@ -9,5 +9,34 @@ public class AppDocumento{
         d1.exibirDados();
         d1.setStatus(Documento.AGUARDANDO_AUTORIZACAO);
         d1.exibirDados();
+
+        d2.exibirDados();
+        d3.exibirDados();
+    }
+}
+
+class Documento {
+    public static final String AGUARDANDO_AUTORIZACAO = "Aguardando autorizacao";
+    public static final String AUTORIZADO = "Autorizado";
+    public static final String REJEITADO = "Rejeitado";
+
+    private int id;
+    private String tipo;
+    private String status;
+
+    public Documento(int id, String tipo) {
+        this.id = id;
+        this.tipo = tipo;
+        this.status = AGUARDANDO_AUTORIZACAO;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void exibirDados() {
+        System.out.println("ID: " + id);
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Status: " + status);
     }
 }
