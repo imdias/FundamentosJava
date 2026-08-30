22db264ca7f33380b1105f99acc4dd0bdc9e9616
Igor Dias <igordias.md@gmail.com>
2026-08-30 17:53:27 -0300

    Commitando primeiros arquivos sobre herança, ainda não concluído, somente iniciado

---
+++ parte2-OrientacaoObjetos/aula206-heranca/Pessoa.java
@@ -0,0 +1,25 @@
+public class Pessoa{
+	private String nome;
+	private String email;
+
+	public Pessoa(String nome, String email){
+		this.nome = nome;
+		this.email = nome;
+	}
+
+	public String getNome(){
+		return this.nome;
+	}
+
+	public String getEmail(){
+		return this.nome;
+	}
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+}
