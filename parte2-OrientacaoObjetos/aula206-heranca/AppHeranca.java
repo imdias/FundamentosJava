95a697c529a18702024eec135932a3c8421f2dbf
Igor Dias <igordias.md@gmail.com>
2026-08-31 05:28:15 -0300

    Alterando o construtor

--- parte2-OrientacaoObjetos/aula206-heranca/AppHeranca.java
+++ parte2-OrientacaoObjetos/aula206-heranca/AppHeranca.java
@@ -1,13 +1,8 @@
 public class AppHeranca{
 	public static void main(String args[]){
-		Pessoa p = new Pessoa();
-		p.setNome("Isidro");
-		p.setEmail("isidro@isidro.com");
+		Pessoa p = new Pessoa("Isidro", "isidro@isidro.com");
 
-		Funcionario f = new Funcionario();
-		f.setNome("Josineidson");
-		f.setEmail("josineidson@isidro.com");
-		f.setSalario(5000);
+		Funcionario f = new Funcionario("Josineidson", "josineidson@isidro.com", 5000);
 
 		System.out.println("Mostrando infos....");
 		System.out.println("Pessoa: " + p.getNome() + " - " + p.getEmail());
