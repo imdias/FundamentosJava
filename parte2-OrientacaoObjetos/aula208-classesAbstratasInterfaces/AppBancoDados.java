c9ecc4fec6a4e820220437ae8b684518d851b6ae
Igor Dias <igordias.md@gmail.com>
2026-09-04 06:09:56 -0300

    AppBancoDados.java, implementação

---
+++ parte2-OrientacaoObjetos/aula208-classesAbstratasInterfaces/AppBancoDados.java
@@ -0,0 +1,13 @@
+public class AppBancoDados{
+	public static void main(String args[]){
+		InterfaceBD x;
+		if (args[0].equals("mysql")) {
+			x = new DriverMySQL();		
+				}
+		else{
+			x = new DriverOracle();
+		}
+		x.conectar("localhost");
+		x.executar("SELECT * from tabela \n");
+	}
+}
