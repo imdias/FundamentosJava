e9bf22bd0d4956e019a8b6531044c2ee2fb84a96
Igor Dias <igordias.md@gmail.com>
2026-09-04 05:20:56 -0300

    Locadora utilizando classe abstrata

--- parte2-OrientacaoObjetos/aula208-classesAbstratasInterfaces/Locadora.java
+++ parte2-OrientacaoObjetos/aula208-classesAbstratasInterfaces/Locadora.java
@@ -1,5 +1,17 @@
 public class Locadora{
 	public static void main(String args[]){
-		Veiculo v = new Carro("Celta","JDK0001");
+		Veiculo estoque[];
+
+		estoque = new Veiculo[5];
+
+		estoque[0] = new Carro("Celta","JDK 0001", 50f);
+		estoque[1] = new Moto("Honda PCX", "JRE 0002", 25f, 5f);
+		estoque[2] = new Quadriciclo("Polar MX1", "MAC 003", 100.0f, 400.0f);
+		estoque[3] = new Carro("Mercedes", "WIN 0004", 250f);
+		estoque[4] = new Moto("Kawa Ninja ZX10", "ISI 0005", 600f, 2f);
+
+		for (Veiculo v: estoque) {
+			System.out.println("Veiculo v = " + v.getModelo() + " Aluguel = R$ " + v.calcularAluguel(7));
+		}
 	}
 }
