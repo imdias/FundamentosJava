c8400dba336a3731e0f83104ea226a6086bd631f
Igor Dias <igordias.md@gmail.com>
2026-09-05 17:31:16 -0300

    Tratamento via multi catch

--- parte3-ArquivoseExtras/TesteExceptions.java
+++ parte3-ArquivoseExtras/TesteExceptions.java
@@ -8,11 +8,21 @@ public class TesteExceptions{
 
 		System.out.print("Resultado da divisão = " + c + "\n");
 	}
-	catch(ArrayIndexOutOfBoundsException ex){
-		System.err.print("Plis, informe 2 valores \n");
+	catch(ArrayIndexOutOfBoundsException | NumberFormatException ex){
+		System.err.print("Plis, 2 valores INTEIROS!!! \n");
+	}
+	/*catch(NumberFormatException ex){
+		System.err.print("Plis, 2 valores INTEIROS!!! \n");
+	}*/
+	catch(ArithmeticException ex){
+		System.err.print("Não da pra dividir por ZEROOOOOOOO!!! X( \n");
 	}
 	catch(Exception ex){
 		System.err.print("Deu ruim!!! \n");		
-		}
+	}
+	finally{
+		System.out.print("Chegue no finally... posso fazer algo aqui \n");
+	}
+
 	}
 }
