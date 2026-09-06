8eae93f7d1ac8148267f34d151ec70a7bde8e627
Igor Dias <igordias.md@gmail.com>
2026-09-06 07:17:59 -0300

    Criando arquivo de leitura

---
+++ parte3-ArquivoseExtras/LeituraDeArquivo.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class LeituraDeArquivo{
+	public static void main(String args[]) throws Exception{
+		BufferedReader br = null;
+		try{
+			br = new BufferedReader(new FileReader("arquivo.in"));
+			String s;
+			while((s = br.readLine()) != null){
+				System.out.print("Lido = " + s + " \n");
+			}
+		}
+		catch(IOException ex){
+			System.err.print("Deu algum problema");
+		}
+		finally{
+			if (br != null) {
+				br.close();
+			}
+		}
+	}	
+}
