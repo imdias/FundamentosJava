71aeb0c49d9d914878447ea255baf35eebe2501f
Igor Dias <igordias.md@gmail.com>
2026-09-06 07:24:27 -0300

    Implementando try with resources

--- parte3-ArquivoseExtras/LeituraDeArquivo.java
+++ parte3-ArquivoseExtras/LeituraDeArquivo.java
@@ -2,9 +2,9 @@ import java.io.*;
 
 public class LeituraDeArquivo{
 	public static void main(String args[]) throws Exception{
-		BufferedReader br = null;
-		try{
-			br = new BufferedReader(new FileReader("arquivo.in"));
+		//BufferedReader br = null;
+		try(BufferedReader br = new BufferedReader(new FileReader("arquivo.in"));){
+		//	br = new BufferedReader(new FileReader("arquivo.in"));
 			String s;
 			while((s = br.readLine()) != null){
 				System.out.print("Lido = " + s + " \n");
@@ -13,10 +13,15 @@ public class LeituraDeArquivo{
 		catch(IOException ex){
 			System.err.print("Deu algum problema");
 		}
+		
+		//
+		/*
 		finally{
 			if (br != null) {
 				br.close();
 			}
 		}
+		*/
+
 	}	
 }
