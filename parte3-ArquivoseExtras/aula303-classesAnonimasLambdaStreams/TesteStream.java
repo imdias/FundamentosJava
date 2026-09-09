3169dc2a37794c7ac3c96f73ae59412d04d94756
Igor Dias <igordias.md@gmail.com>
2026-09-09 05:55:24 -0300

    continução de exemplos com Stream

--- parte3-ArquivoseExtras/aula303-classesAnonimasLambdaStreams/TesteStream.java
+++ parte3-ArquivoseExtras/aula303-classesAnonimasLambdaStreams/TesteStream.java
@@ -9,11 +9,26 @@ public class TesteStream{
 		list = Arrays.asList(1,2,3,4,5,6,7,8,9,9,9,9,9,9,9,8,7,6,5,4,3,2,1);
 		
 		list.stream() // transformo para um "fluxo de dados"
-			//.distinct() // elimina as duplicadas
+			.distinct() // elimina as duplicadas
 			//.skip(n)   // pula n elementos
 			//.limit(n) // seleciona os n primeiros elementos  
-			//.filter(e -> e % 2 == 0 && e < 5)   
+			.filter(e -> e % 2 == 0)   
 			//.map(e -> e * 10)
-			//.forEach(e -> System.out.println(e));
+			.forEach(e -> System.out.println(e));
+
+
+
+		//Long contador = list.stream()
+		//						.count();
+
+		//System.out.println("Numero de elementos " + contador);
+
+		Integer soma = list.stream()
+						   .filter(e -> e % 2 == 0)
+						   .distinct()
+						   .mapToInt(Integer::intValue)
+						   .sum();
+
+		System.out.println("Valor da soma = " + soma);
 	}
 }
