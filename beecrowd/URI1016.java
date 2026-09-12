cdb1cfb29ad2cbc4367da30deb503fee57b07ea6
Igor Dias <igordias.md@gmail.com>
2026-09-12 19:21:00 -0300

    URI1016

---
+++ beecrowd/URI1016.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class URI1016{
+    public static void main(String args[]){
+        Scanner teclado = new Scanner(System.in);
+
+        int km = teclado.nextInt();
+
+        int tempo = km * 2;
+
+        System.out.println(tempo + " minutos");
+    }
+}
