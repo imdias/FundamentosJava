8ec07422b41d7aee3171054eb86a4af20c34c46b
Igor Dias <igordias.md@gmail.com>
2026-09-18 05:25:28 -0300

    Solução URI1009 em Java

--- beecrowd/URI1009.java
+++ beecrowd/URI1009.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class URI1009{
 	public static void main(String args[]){
-	
+		Scanner teclado = new Scanner (System.in);
+
+		String vendedor;
+		float salario, vendas, total;
+
+		vendedor = teclado.nextLine();
+		salario = teclado.nextFloat();
+		vendas = teclado.nextFloat();
+
+		total = salario + (vendas * 0.15f);
+
+		System.out.printf("TOTAL = %.2f%n", total);
+
 	}
 }
