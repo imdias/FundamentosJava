a56636b741ba68766e7312c7a61b86bea979df52
Igor Dias <igordias.md@gmail.com>
2026-09-18 05:35:46 -0300

    Exercício corrigido, formato aceito pelo Beecrowd

--- beecrowd/URI1009.java
+++ beecrowd/URI1009.java
@@ -3,17 +3,11 @@ import java.util.Scanner;
 public class URI1009{
 	public static void main(String args[]){
 		Scanner teclado = new Scanner (System.in);
-
-		String vendedor;
-		float salario, vendas, total;
-
-		vendedor = teclado.nextLine();
-		salario = teclado.nextFloat();
-		vendas = teclado.nextFloat();
-
-		total = salario + (vendas * 0.15f);
-
-		System.out.printf("TOTAL = %.2f%n", total);
+		String vendedor = teclado.nextLine();
+		double salario = teclado.nextFloat();
+		double vendas = teclado.nextFloat();
+		double total = salario + (vendas * 0.15f);
+		System.out.println(String.format("TOTAL = R$ %.2f", total));
 
 	}
 }
