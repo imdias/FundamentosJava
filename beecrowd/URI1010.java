35f56324f9a841a12307d859aefe89bb14f2deb8
Igor Dias <igordias.md@gmail.com>
2026-09-22 04:27:55 -0300

    Resolução exercício URI1010.java do site beecrowd

--- beecrowd/URI1010.java
+++ beecrowd/URI1010.java
@@ -1,5 +1,24 @@
-public class URI1010{
-	public static void (String args[]){
-	
+import java.util.Scanner;
+
+public class URI1010 {
+	public static void main (String args[]) {
+		Scanner teclado = new Scanner (System.in);
+
+		int cod1, cod2, peca1, peca2, qtd1, qtd2;
+		float valor1, valor2, total;
+
+
+		cod1 = teclado.nextInt();
+		peca1 = teclado.nextInt();
+		valor1 = teclado.nextFloat();
+
+		cod2 = teclado.nextInt();
+		peca2 = teclado.nextInt();
+		valor2 = teclado.nextFloat();
+
+		total = (peca1 * valor1) + (peca2 * valor2);
+
+		System.out.printf("VALOR A PAGAR: R$ %.2f\n", total);
+
 	}
 }
